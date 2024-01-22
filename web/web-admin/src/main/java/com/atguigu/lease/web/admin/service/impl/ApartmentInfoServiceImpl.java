9b8185d6fa2408b65f53912419f89faa6dbd50d1
simonf2000 <simonf2000@outlook.com>
2024-01-22 21:32:54 +0800

    保存或更新公寓信息,简化了部分代码。

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/ApartmentInfoServiceImpl.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/ApartmentInfoServiceImpl.java
@@ -1,10 +1,26 @@
 package com.atguigu.lease.web.admin.service.impl;
 
-import com.atguigu.lease.model.entity.ApartmentInfo;
+import com.atguigu.lease.model.entity.*;
+import com.atguigu.lease.model.enums.ItemType;
 import com.atguigu.lease.web.admin.mapper.ApartmentInfoMapper;
-import com.atguigu.lease.web.admin.service.ApartmentInfoService;
+import com.atguigu.lease.web.admin.service.*;
+import com.atguigu.lease.web.admin.vo.apartment.ApartmentSubmitVo;
+import com.atguigu.lease.web.admin.vo.graph.GraphVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import kotlin.jvm.internal.Lambda;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 /**
  * @author liubo
@@ -14,6 +30,101 @@ import org.springframework.stereotype.Service;
 @Service
 public class ApartmentInfoServiceImpl extends ServiceImpl<ApartmentInfoMapper, ApartmentInfo>
         implements ApartmentInfoService {
+    @Autowired
+    private GraphInfoService graphInfoService;
+    @Autowired
+    private ApartmentFacilityService apartmentFacilityService;
+    @Autowired
+    private ApartmentLabelService apartmentLabelService;
+    @Autowired
+    private ApartmentFeeValueService apartmentFeeValueService;
+
+    @Override
+    public void saveOrUpdateApartment(ApartmentSubmitVo apartmentSubmitVo) {
+        boolean isUpdate = apartmentSubmitVo.getId() != null;
+        saveOrUpdate(apartmentSubmitVo);
+        if (isUpdate) {
+            // 删除GraphInfo
+            LambdaQueryWrapper<GraphInfo> graphQueryWrapper = new LambdaQueryWrapper<>();
+            graphQueryWrapper.eq(GraphInfo::getItemType, ItemType.APARTMENT);
+            graphQueryWrapper.eq(GraphInfo::getItemId,apartmentSubmitVo.getId());
+            graphInfoService.remove(graphQueryWrapper);
+            // 删除ApartmentFacility
+            removeItemsByField(ApartmentFacility.class, ApartmentFacility::getApartmentId, apartmentSubmitVo.getId(), apartmentFacilityService);
+
+            // 删除ApartmentLabel
+            removeItemsByField(ApartmentLabel.class, ApartmentLabel::getApartmentId, apartmentSubmitVo.getId(), apartmentLabelService);
+
+            // 删除ApartmentFeeValue
+            removeItemsByField(ApartmentFeeValue.class, ApartmentFeeValue::getApartmentId, apartmentSubmitVo.getId(), apartmentFeeValueService);
+        }
+
+        //1.插入图片列表
+        List<GraphVo> graphVoList = apartmentSubmitVo.getGraphVoList();
+        if (!CollectionUtils.isEmpty(graphVoList)){
+            ArrayList<GraphInfo> graphInfoList = new ArrayList<>();
+            for (GraphVo graphVo : graphVoList) {
+                GraphInfo graphInfo = new GraphInfo();
+                graphInfo.setItemType(ItemType.APARTMENT);
+                graphInfo.setItemId(apartmentSubmitVo.getId());
+                graphInfo.setName(graphVo.getName());
+                graphInfo.setUrl(graphVo.getUrl());
+                graphInfoList.add(graphInfo);
+            }
+            graphInfoService.saveBatch(graphInfoList);
+        }
+
+
+        //2.插入配套列表
+        List<Long> facilityInfoIdList = apartmentSubmitVo.getFacilityInfoIds();
+        if (!CollectionUtils.isEmpty(facilityInfoIdList)){
+            ArrayList<ApartmentFacility> facilityList = new ArrayList<>();
+            for (Long facilityId : facilityInfoIdList) {
+                ApartmentFacility apartmentFacility = ApartmentFacility.builder().apartmentId(apartmentSubmitVo.getId())
+                        .facilityId(facilityId).build();
+                facilityList.add(apartmentFacility);
+            }
+            apartmentFacilityService.saveBatch(facilityList);
+        }
+
+
+        //3.插入标签列表
+        List<Long> labelIds = apartmentSubmitVo.getLabelIds();
+        if (!CollectionUtils.isEmpty(labelIds)) {
+            List<ApartmentLabel> apartmentLabelList = new ArrayList<>();
+            for (Long labelId : labelIds) {
+                ApartmentLabel apartmentLabel = ApartmentLabel.builder().apartmentId(apartmentSubmitVo.getId())
+                        .labelId(labelId).build();
+                apartmentLabelList.add(apartmentLabel);
+            }
+            apartmentLabelService.saveBatch(apartmentLabelList);
+        }
+
+
+        //4.插入杂费列表
+        List<Long> feeValueIds = apartmentSubmitVo.getFeeValueIds();
+        if (!CollectionUtils.isEmpty(feeValueIds)) {
+            ArrayList<ApartmentFeeValue> apartmentFeeValueList = new ArrayList<>();
+            for (Long feeValueId : feeValueIds) {
+                ApartmentFeeValue apartmentFeeValue = ApartmentFeeValue.builder().apartmentId(apartmentSubmitVo.getId())
+                        .feeValueId(feeValueId).build();
+                apartmentFeeValueList.add(apartmentFeeValue);
+            }
+            apartmentFeeValueService.saveBatch(apartmentFeeValueList);
+        }
+
+    }
+
+
+    private <T, R> void removeItemsByField(Class<T> clazz, SFunction<T, R> fieldGetter, R fieldValue, IService<T> service) {
+        LambdaQueryWrapper<T> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(fieldGetter, fieldValue);
+        service.remove(queryWrapper);
+    }
+
+
+
+
 
 }
 
