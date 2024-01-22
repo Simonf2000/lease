ec656f574eaf84d9334bd300d1f92e9dc3040780
simonf2000 <simonf2000@outlook.com>
2024-01-22 10:48:59 +0800

    1.完善查询全部属性名称和属性值列表功能(添加mapper.xml中多表查询sql语句)，修复已知bug 2.添加根据ID删除属性名称功能 3.添加根据ID删除属性值功能

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/AttrController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/AttrController.java
@@ -7,6 +7,7 @@ import com.atguigu.lease.model.entity.AttrValue;
 import com.atguigu.lease.web.admin.service.AttrKeyService;
 import com.atguigu.lease.web.admin.service.AttrValueService;
 import com.atguigu.lease.web.admin.vo.attr.AttrKeyVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,12 +52,18 @@ public class AttrController {
     @Operation(summary = "根据id删除属性名称")
     @DeleteMapping("key/deleteById")
     public Result removeAttrKeyById(@RequestParam Long attrKeyId) {
+        attrKeyService.removeById(attrKeyId);
+        LambdaQueryWrapper<AttrValue> attrValueLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        attrValueLambdaQueryWrapper.eq(AttrValue::getAttrKeyId,attrKeyId);
+        attrValueService.remove(attrValueLambdaQueryWrapper);
         return Result.ok();
     }
 
     @Operation(summary = "根据id删除属性值")
     @DeleteMapping("value/deleteById")
     public Result removeAttrValueById(@RequestParam Long id) {
+
+        attrValueService.removeById(id);
         return Result.ok();
     }
 
