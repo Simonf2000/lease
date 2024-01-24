ae5d3337fea07e9e50b3357eb8cd2e0953264df0
simonf2000 <simonf2000@outlook.com>
2024-01-24 10:23:25 +0800

    1. 根据ID删除房间信息新增租约是否为空判断。 2. Spring日期全局配置。

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/RoomInfoServiceImpl.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/RoomInfoServiceImpl.java
@@ -1,5 +1,7 @@
 package com.atguigu.lease.web.admin.service.impl;
 
+import com.atguigu.lease.common.exception.LeaseException;
+import com.atguigu.lease.common.result.ResultCodeEnum;
 import com.atguigu.lease.model.entity.*;
 import com.atguigu.lease.model.enums.ItemType;
 import com.atguigu.lease.web.admin.mapper.RoomInfoMapper;
@@ -30,14 +32,33 @@ public class RoomInfoServiceImpl extends ServiceImpl<RoomInfoMapper, RoomInfo>
     @Autowired
     private RoomLeaseTermService roomLeaseTermService;
 
+    @Autowired
+    private LeaseAgreementService leaseAgreementService;
+
     /**
-    * @Param: [id]
-    * @return: void
-    * @Author: simonf
-    * @Date: 2024/1/23
-    */
+     * @Description: 先看看lease_agreement里面有没有租约(租约状态是2 ， 5)，没有租约再删除房间
+     * @Param: [id]
+     * @return: void
+     * @Author: simonf
+     * @Date: 2024/1/24
+     */
     @Override
     public void customRemoveRoomById(Long id) {
+        //先看看lease_agreement里面有没有租约(租约状态是2，5)
+
+        // 1.看看房间里面有没有租约
+        LambdaQueryWrapper<LeaseAgreement> leaseAgreementIfNull = new LambdaQueryWrapper<>();
+        leaseAgreementIfNull.eq(LeaseAgreement::getRoomId, id);
+        Long count = leaseAgreementService.count(leaseAgreementIfNull);
+
+        // 2.看看租约状态
+        LambdaQueryWrapper<LeaseAgreement> leaseAgreementQueryWrapper = new LambdaQueryWrapper<>();
+        leaseAgreementQueryWrapper.eq(LeaseAgreement::getStatus, 2);
+        leaseAgreementQueryWrapper.eq(LeaseAgreement::getStatus, 5);
+        if (count > 0) {
+            throw new LeaseException(ResultCodeEnum.DELETE_ERROR);
+        }
+
         //1.删除RoomInfo
         super.removeById(id);
 
