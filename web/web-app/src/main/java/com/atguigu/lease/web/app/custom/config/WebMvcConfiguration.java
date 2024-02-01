8779fc031276b66d18173e875f8c2042da78c4a3
simonf2000 <simonf2000@outlook.com>
2024-02-01 15:27:45 +0800

    web-app-租约管理-根据ID更新租约状态

--- web/web-app/src/main/java/com/atguigu/lease/web/app/custom/config/WebMvcConfiguration.java
+++ web/web-app/src/main/java/com/atguigu/lease/web/app/custom/config/WebMvcConfiguration.java
@@ -1,9 +1,11 @@
 package com.atguigu.lease.web.app.custom.config;
 
+import com.atguigu.lease.web.app.custom.converter.StringToBaseEnumConverterFactory;
 import com.atguigu.lease.web.app.custom.interceptor.AuthenticationInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,6 +27,13 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Autowired
     private AuthenticationInterceptor authenticationInterceptor;
+    @Autowired
+    private StringToBaseEnumConverterFactory stringToBaseEnumConverterFactory;
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverterFactory(this.stringToBaseEnumConverterFactory);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
